c2789eb8bee40bb3add6676c88f2f3361225e048
Keegan Rubingh <keeganrubingh@gmail.com>
2025-05-28 18:52:11 -0500

    LfeedChanges

--- lib/py/LiveFeed.py
+++ lib/py/LiveFeed.py
@@ -1,20 +1,47 @@
+import mss.base
+import mss.darwin
+import mss.factory
 import numpy as np
 import cv2 as cv
 import FlowerScan as processor
 import easygui
 import threading
+import mss
+import win32gui
  
-def rescale_frame(frame, scale):    # works for image, video, live video
-    width = int(frame.shape[1] * scale)
-    height = int(frame.shape[0] * scale)
-    dimensions = (width, height)
-    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+ 
+source = easygui.choicebox("Set video source",choices=[
+    "Whole Monitor (MUST BE FULLSCREEN)",
+    "OBS Virtual Camera"
+])
 
 
-skipCamera = False
-cameraToSkipTo = 1
+getFrame = lambda:()
+cap = None
+window = win32gui.FindWindow(None,"Natro Macro")
+width = win32gui.GetWindowRect(window)
+print(width)
 
-if(not skipCamera):
+if(source == "Whole Monitor (MUST BE FULLSCREEN)"):
+    sct = mss.mss()
+    choices=sct.monitors[1:]
+    cameraNum = choices[0]
+    choices[:,0]
+
+    if(len(choices) >= 2):
+        contSelect = True
+        while(contSelect):
+            cameraNum = easygui.choicebox("Select Monitor",choices=choices)
+            
+            if(cameraNum == None):
+                print("User cancelled monitor selection!")
+                exit()
+            
+            screenshot = np.array(sct.grab(cameraNum))
+            cv.imshow('Camera', screenshot)
+            contSelect = not easygui.boolbox("Is this camera correct?","Camera Select")
+    getFrame = lambda:(np.array(sct.grab(cameraNum)))
+elif(source == "OBS Virtual Camera"):
     camWin = None
     contSelect = True
     while(contSelect):
@@ -26,35 +53,30 @@ if(not skipCamera):
             easygui.msgbox("That camera does not exist!")
             continue
         
+        cap.set(cv.CAP_PROP_FRAME_WIDTH)
+        
         ret,frame = cap.read()
+        print(frame.shape)
         cv.imshow('Camera', frame)
         contSelect = not easygui.boolbox("Is this camera correct?","Camera Select")
-else:
-    cap = cv.VideoCapture(cameraToSkipTo)
+    getFrame = lambda:(cap.read()[1])
+
+    
 cv.destroyAllWindows()
-
-cap.set(cv.CAP_PROP_FRAME_WIDTH,1920)
-cap.set(cv.CAP_PROP_FRAME_HEIGHT,1080)
-
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
 
 while True:
     # Capture frame-by-frame
-    ret, frame = cap.read()
- 
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+    frame = getFrame()
+    
     # Our operations on the frame come here
-    out = processor.processScreenShot(frame)
+    # out = processor.processScreenShot(frame)
+    out = frame
     # Display the resulting frame
     cv.imshow('Processed Stream', out)
     if cv.waitKey(1) == ord('q'):
         break
  
 # When everything done, release the capture
-cap.release()
+if(cap):
+    cap.release()
 cv.destroyAllWindows()
